61604f115b3bc6aa85dc184dadd752e9794e76c6
msmenezes <msmenezes@inf.ufpel.edu.br>
2024-10-29 21:00:25 -0300

    Some changes on Options

--- src/views/OptionsView.vue
+++ src/views/OptionsView.vue
@@ -11,74 +11,92 @@
 
         <div class="sm:col-span-3 w-fit">
           <div class="flex flex-col gap-4 px-2">
-            <div>
-              <span class="text-2xl">
+            <div class="flex flex-col gap-2 text-2xl">
+              <span class="font-semibold">
                 O que deseja modificar?
               </span>
+              <span class="w-5/6 text-lg font-light">
+                (Será possível mudar essa escolha mais tarde na opção "Redefinir o tipo de mudança")
+              </span>
             </div>
-            <div class="flex bg-gray-100 border-black border-2 p-4">
-              <form id="myForm" class="flex flex-col p-4 gap-2" action="editor" method="get">
-                <div>
-                  <label>
-                    <input type="radio" name="opcao" value="opcao"
-                      onclick="enableCheckboxes('opcao1'); clearSelection()">
-                    <span class="font-semibold"> Aumentar sala/cozinha</span>
-                  </label>
-                  <div class="flex flex-col px-4 gap-1" id="checkboxes1">
-                    <label>
-                      <input type="checkbox" name="modelo1_a" value="modelo1_a"> Para a frente</label>
-                    <label>
-                      <input type="checkbox" name="modelo1_b" value="modelo1_b"> Para a frente e lado</label>
-                    <label>
-                      <input type="checkbox" name="modelo1_c" value="modelo1_c"> Para o lado</label>
-                    <label>
-                      <input type="checkbox" name="modelo1_d" value="modelo1_d"> Acrescentar área de serviço</label>
-                  </div>
-                </div>
+            <div class="flex bg-gray-100 border-black border-2 p-4 ">
+              <form id="myForm" class="flex flex-row justify-between w-full" action="editor" method="get">
 
-                <div>
-                  <label>
-                    <input type="radio" name="opcao" value="opcao"
-                      onclick="enableCheckboxes('opcao2'); clearSelection()">
-                    <span class="font-semibold"> Acrescentar cômodo para os fundos</span>
-                  </label>
-                  <div class="flex flex-col px-4 gap-1" id="checkboxes2">
-                    <label>
-                      <input type="checkbox" name="modelo2_a" value="modelo2_a"> Acesso independente
-                    </label>
-                    <label>
-                      <input type="checkbox" name="modelo2_b" value="modelo2_b"> Acrescentar banheiro
-                    </label>
-                  </div>
-                </div>
+                <div class="flex flex-col p-4 gap-2">
 
-                <div>
-                  <label>
-                    <input type="radio" name="opcao" value="opcao">
-                    <span class="font-semibold"> Acrescentar cômodo para a frente</span>
-                  </label>
-                  <div class="flex flex-col px-4 gap-1" id="checkboxes3">
+                  <div>
                     <label>
-                      <input type="checkbox" name="modelo3_a" value="modelo3_a"> Acesso independente
+                      <input type="radio" name="opcao" value="opcao" class="disabled:font-light disabled:text-gray-600 disabled:cursor-not-allowed">
+                      <label class="font-semibold"> Aumentar sala/cozinha</label>
                     </label>
-                    <label>
-                      <input type="checkbox" name="modelo3_b" value="modelo3_b"> Acrescentar área de serviço
-                    </label>
+                    <div class="flex flex-col px-4 gap-1" id="checkboxes1">
+                      <label>
+                        <input type="checkbox" name="modelo1_a" value="modelo1_a"> Para a frente</label>
+                      <label>
+                        <input type="checkbox" name="modelo1_b" value="modelo1_b"> Para a frente e lado</label>
+                      <label>
+                        <input type="checkbox" name="modelo1_c" value="modelo1_c"> Para o lado</label>
+                      <label>
+                        <input type="checkbox" name="modelo1_d" value="modelo1_d"> Acrescentar área de serviço</label>
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <label>
-                    <input type="radio" name="opcao" value="opcao"
-                      onclick="enableCheckboxes('opcao4'); clearSelection()">
+                  <div>
+                    <label>
+                      <input type="radio" name="opcao" value="opcao"
+                        onclick="enableCheckboxes('opcao2'); clearSelection()">
+                      <span class="font-semibold"> Acrescentar cômodo para os fundos</span>
+                    </label>
+                    <div class="flex flex-col px-4 gap-1" id="checkboxes2">
+                      <label>
+                        <input type="checkbox" name="modelo2_a" value="modelo2_a"> Acesso independente
+                      </label>
+                      <label>
+                        <input type="checkbox" name="modelo2_b" value="modelo2_b"> Acrescentar banheiro
+                      </label>
+                    </div>
+                  </div>
+
+                  <div>
+                    <label>
+                      <input type="radio" name="opcao" value="opcao">
+                      <span class="font-semibold"> Acrescentar cômodo para a frente</span>
+                    </label>
+                    <div class="flex flex-col px-4 gap-1" id="checkboxes3">
+                      <label>
+                        <input type="checkbox" name="modelo3_a" value="modelo3_a"> Acesso independente
+                      </label>
+                      <label>
+                        <input type="checkbox" name="modelo3_b" value="modelo3_b"> Acrescentar área de serviço
+                      </label>
+                    </div>
+                  </div>
+
+                  <div>
+                    <label>
+                      <input type="radio" name="opcao" value="opcao"
+                        onclick="enableCheckboxes('opcao4'); clearSelection()">
                       <span class="font-semibold"> Acrescentar Pavimento</span>
-                  </label>
-                  <div class="flex flex-col px-4 gap-1" id="checkboxes4">
-                    <label>
-                      <input type="checkbox" name="modelo4" value="modelo4"> Acrescentar banheiro</label>
+                    </label>
+                    <div class="flex flex-col px-4 gap-1" id="checkboxes4">
+                      <label>
+                        <input type="checkbox" name="modelo4" value="modelo4"> Acrescentar banheiro</label>
+                    </div>
                   </div>
+                  <input type="submit" class="btn btnform" value="Editar">
+
                 </div>
-                <input type="submit" class="btn btnform" value="Editar">
+                <div>
+                </div>
+                <button type="reset" class="w-fit h-fit" >
+                  <div class="rounded-full bg-black w-fit h-fit p-4 ease-in-out duration-200 hover:animate-spin"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                      viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                  </div> 
+                </button>
               </form>
             </div>
             <!-- <fieldset aria-label="Opções de modificação">
