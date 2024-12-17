4c86cc4566ab94e99f299715736746bc78975ba5
msmenezes <msmenezes@inf.ufpel.edu.br>
2024-12-17 18:27:00 -0300

    Added the necessary files for preview

--- src/views/OptionsView.vue
+++ src/views/OptionsView.vue
@@ -4,15 +4,14 @@
       <div class="grid grid-cols-1 sm:grid-cols-5 gap-8 items-center h-full w-full">
         <!-- Imagem da capa com desfoque -->
         <div
-          class="relative sm:col-span-2 flex justify-center sm:border-b-2 md:border-r-2 border-black sm:justify-start h-full w-full">
+          class="relative sm:col-span-2 flex justify-center align-middle sm:border-b-2 md:border-r-2 border-black sm:justify-start h-full w-full">
           <!-- Image with blur effect -->
-          <img alt="casa" src="/casa.png?url" class="w-full h-full object-cover filter blur-sm" />
-
-          <!-- Text overlay (selectedInfo) -->
-          <div
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-extrabold z-10">
-            {{ displayId }}
-          </div>
+          <img 
+            alt="casa" 
+            :src="imagePath" 
+            @error="handleImageError" 
+            :class="imagePath === 'casa.png' ? 'object-cover' : 'object-scale-down'" 
+          />
         </div>
 
         <div class="sm:col-span-3 w-fit">
@@ -27,7 +26,6 @@
 
 <script>
 import { ref, watch } from 'vue';
-import optionsData from '@/data/optionsData.json';
 import options from '@/data/options.json'
 import Selector from '@/components/Selector.vue';
 
@@ -38,21 +36,45 @@ export default {
   setup() {
     const selectedInfo = ref([]);
     const option = ref(options.data);
-    const displayId = ref(null);
+    const displayId = ref("");
+    const imagePath = ref("");
+
+    async function getSelectionId(selection) {
+      const enconder = new TextEncoder();
+      const encodedText = enconder.encode(selection);
+      const hash = await window.crypto.subtle.digest("SHA-256", encodedText);
+      const hashArray = Array.from(new Uint8Array(hash)); // convert buffer to byte array
+      const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join(""); // convert bytes to hex string
+      return hashHex;
+    }
 
     watch(selectedInfo, (newVal) => {
-      if(newVal.length != 0){
+      if (newVal.length != 0) {
         const sortedCombos = [newVal].sort();
         const fileId = btoa(sortedCombos.join(","));
-        displayId.value = fileId; 
-        console.log('DisplayID is: ',displayId.value )
+        getSelectionId(sortedCombos.join(",")).then((uniqueId) =>
+          displayId.value = uniqueId)
+        console.log('DisplayID is: ', displayId.value)
       }
     });
+
+    watch(displayId, (newVal) => {
+      const basePath = "/images/"
+      imagePath.value = basePath + newVal + ".png";
+    });
+
+    const handleImageError = () => {
+      imagePath.value = "/images/base.png"; // Set the backup image when the original fails
+    };
 
     return {
       selectedInfo,
       displayId,
       option,
+      imagePath,
+      handleImageError,
     };
   }
 };
